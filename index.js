5478f0ab582c5f0c51963ac1a1498bb996dc4d77
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-08-12 15:03:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+// Create MySQL connection pool
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'mehedi86',
+  database: 'jobdb'
+});
+
+// all users
+app.get('/api/users', (req, res) => {
+  pool.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      console.error('Error fetching users:', err);
+      return res.status(500).json({ error: 'Database query error' });
+    }
+    res.json(results);
+  });
+});
+
+
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
